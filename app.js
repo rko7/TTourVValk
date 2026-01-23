bc82fc09b642de32ed6382bad4f64d37d371974a
Serene Ko <hazel.r.ko@gmail.com>
2026-01-22 22:43:57 -0500

    use current location

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let map;
 let selectedLatLng = null;
 let selectedPin = null;
 
-// Initial map center (Toronto) – user-selected location will override this
+//// Initial map center (Toronto) – user-selected location will override this
 function initMap() {
   const defaultCenter = { lat: 43.6532, lng: -79.3832 };
 
@@ -13,13 +13,42 @@ function initMap() {
 
   map.addListener("click", (e) => {
     selectedLatLng = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    placeSelectedPin(selectedLatLng);
+  });
 
-    if (selectedPin) selectedPin.setMap(null);
-    selectedPin = new google.maps.Marker({
-      map,
-      position: selectedLatLng,
-      title: "Selected location",
-    });
+  document
+    .getElementById("use-current-location")
+    .addEventListener("click", handleUseCurrentLocation);
+}
+
+function handleUseCurrentLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation not supported.");
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      selectedLatLng = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+      map.setCenter(selectedLatLng);
+      map.setZoom(15);
+      placeSelectedPin(selectedLatLng);
+    },
+    () => {
+      alert("Could not access location.");
+    }
+  );
+}
+
+function placeSelectedPin(latLng) {
+  if (selectedPin) selectedPin.setMap(null);
+  selectedPin = new google.maps.Marker({
+    map,
+    position: latLng,
+    title: "Selected location",
   });
 }
 
