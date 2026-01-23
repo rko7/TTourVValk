89852b56b59b95ed8be8efe69ed8ef4f8a50fa00
Serene Ko <hazel.r.ko@gmail.com>
2026-01-22 22:23:37 -0500

    pick location

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 let map;
+let selectedLatLng = null;
+let selectedPin = null;
 
 function initMap() {
-  const defaultCenter = { lat: 43.6532, lng: -79.3832 }; // Toronto
+  const defaultCenter = { lat: 43.6532, lng: -79.3832 };
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: defaultCenter,
     zoom: 13,
+  });
+
+  map.addListener("click", (e) => {
+    selectedLatLng = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+
+    if (selectedPin) selectedPin.setMap(null);
+    selectedPin = new google.maps.Marker({
+      map,
+      position: selectedLatLng,
+      title: "Selected location",
+    });
   });
 }
 
